537f19aeb2762eef448e0edd3e293d0a80342082
jamesjones291986 <59849233+jamesjones291986@users.noreply.github.com>
2022-05-01 00:22:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 # AWS Instance
 
+provider "aws" {
+  region = lookup(var.aws, "region")
+}
 resource "aws_instance" "server" {
   ami           = lookup(var.aws, "ami")
   instance_type = lookup(var.aws, "instance_type")
-  availability_zone = lookup(var.aws, "region")
 
   tags = {
     Name = lookup(var.aws, "instance_name")
