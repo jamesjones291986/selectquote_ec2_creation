7bdcc024ef0d03671844fb8e15d831e5a069d6f7
jamesjones291986 <59849233+jamesjones291986@users.noreply.github.com>
2022-05-01 20:59:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = aws_vpc.sq-vpc.cidr_block
+    cidr_blocks = "10.0.0.0/16"
   }
 
   egress {
