00ab46362ed63cecd9923569f8d7d4476b624da2
jamesjones291986 <59849233+jamesjones291986@users.noreply.github.com>
2022-05-02 10:43:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,10 +38,20 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.sq-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "sq-subnet"
+  }
+}
+
 resource "aws_instance" "server" {
   ami           = lookup(var.aws_instance, "ami")
   instance_type = lookup(var.aws_instance, "instance_type")
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id = aws_subnet.subnet.id
   #count = lookup(var.aws_instance, "count")
 
   tags = {
